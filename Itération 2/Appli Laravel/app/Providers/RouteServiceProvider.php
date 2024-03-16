d1f66e27fcb9815aec40cafc301451b686b5402f
BADLVX <79326331+BADLVX@users.noreply.github.com>
2024-03-16 15:15:08 +0100

    modifications des routes
    Ajout des routes permettant d'acceder la page de parking apres la connexion. Ajout des routes enlevé par breeze

--- Itération 2/Appli Laravel/app/Providers/RouteServiceProvider.php
+++ Itération 2/Appli Laravel/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/ParkingAccueil';
 
     /**
      * The controller namespace for the application.
--- Itération 2/Appli Laravel/routes/web.php
+++ Itération 2/Appli Laravel/routes/web.php
@@ -13,12 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', function () { // renvoie la vue apres l'authentification du middleware
+    return view('ParkingAccueil');
+})->middleware(['auth'])->name('dashboard'); //Authentification dans la vue dashboard
 
 require __DIR__.'/auth.php';
+
+Route::get('/reserver', function () {
+    return view('/reserver');
+});
+
+Route::get('/etatreservation', function () {
+    return view('/etatreservation');
+});
+
+Route::get('/anciennereservation', function () {
+    return view('/anciennereservation');
+});
+
+
