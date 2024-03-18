8c8103f031991eca4488ba1c1b972d1090228845
npetronis <144010678+npetronis@users.noreply.github.com>
2024-03-18 10:19:17 +0100

    ajout de la route ParkingAccueil

--- Itération 2/Appli Laravel/routes/web.php
+++ Itération 2/Appli Laravel/routes/web.php
@@ -23,6 +23,10 @@ Route::get('/', function () { // renvoie la vue apres l'authentification du midd
 
 require __DIR__.'/auth.php';
 
+Route::get('/ParkingAccueil', function () {
+    return view('ParkingAccueil');
+});
+
 Route::get('/reserver', function () {
     return view('reserver');
 });
