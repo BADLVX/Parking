ddd85e41b8868cc11b2f4f1ef810290cf03a4e7d
npetronis <nicolaspetronis1@gmail.com>
2024-03-18 14:55:00 +0100

    ajout de listeReservation.blade.php

--- Itération 2/Appli Laravel/app/Http/Controllers/ReservationController.php
+++ Itération 2/Appli Laravel/app/Http/Controllers/ReservationController.php
@@ -4,23 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Reservation;
-use App\Models\User;
-use App\Models\Place;
 
 class ReservationController extends Controller
 {
     public function store(Request $request)
     {   
-        //Validation des données reçues depuis le formulaire de réservation
+        // Validation des données reçues depuis le formulaire de réservation
         $request->validate([
             'date_debut' => 'required|date',
             'date_fin' => 'required|date|after:date_debut',
-            'id_user' => 'required|exists:User,id',
-            'num_place' => 'required|exists:Places, numplace',
+            // Assurez-vous que 'id_user' est valide, vous pouvez utiliser Auth::id() pour l'ID de l'utilisateur actuel
+            'id_user' => 'required|exists:users,id',
+            'num_place' => 'required|exists:places,numplace',
         ]);
 
-        //Création de la réservation
-
+        // Création de la réservation
         $reservation = Reservation::create([
             'date_debut' => $request->date_debut,
             'date_fin' => $request->date_fin,
@@ -28,10 +26,16 @@ class ReservationController extends Controller
             'num_place' => $request->num_place,
         ]);
 
-       
+        // Redirection vers la méthode show() pour afficher la liste des réservations
+        return redirect()->route('reservation.show');
     }
+
     public function show()
     {
-        return view('listeReservation'); // Assume que vous avez une vue nommée 'confirmation.blade.php'
+        // Récupérer les réservations pour affichage
+        $reservations = Reservation::all(); // Vous pouvez modifier cela selon vos besoins
+
+        // Passer les réservations à la vue
+        return view('listeReservation', compact('reservations'));
     }
 }
