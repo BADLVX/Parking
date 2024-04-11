d7e41552b0b0f6aacec352840c8bc37e3a16458d
AlexMezaoui <alezmalou@gmail.com>
2024-04-11 16:16:18 +0200

    Ajout de la page des anciennes reservations
    - Il nous reste à faire la route

--- Itération 2/Appli Laravel/resources/views/parking/user-reservations.blade.php
+++ Itération 2/Appli Laravel/resources/views/parking/user-reservations.blade.php
@@ -1,4 +1,36 @@
+
+
 <!-- user-reservations.blade.php -->
+<html>
+<head>
+    <title>Anciennes Réservations</title>
+<body>
+    <h1>Anciennes Réservations</h1>
+    <ul>
+ Id de l'utiliateur : {{$Iduser = Auth::user()->id}}
+        @foreach($userReservations as $user)
+            @if ( $Iduser == $user->id)
+            <li>
+                Les reservations de {{ $user->name }} sont: 
+                <ul>
+                    @foreach($user->reservations as $reservation)
+
+                            <li>{{ $reservation->id }} - début : {{ $reservation->DateAttribution }} - fin :{{ $reservation->DateFin }} </li>
+                       
+                    @endforeach
+                </ul>
+            </li>
+            @endif
+        @endforeach
+    </ul>
+</body>
+</html>
+
+
+
+<!-- user-reservations.blade.php
+
+afficher les reservations de tous les utilisateurs
 <html>
 <head>
     <title>User Reservations</title>
@@ -18,4 +50,4 @@
         @endforeach
     </ul>
 </body>
-</html>
+</html> -->
